fda9b8f8cbc255776aa69d4c71091eb05ee8086a
Rak65 <rakeshp6505@gmail.com>
2023-05-13 18:51:03 +0530

    Find the maximum number

---
+++ GenericsProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = 850;
+            int num2 = 30;
+            int num3 = 60;
+            Console .WriteLine ("Maximum number is "+ FindMaximum.MaxNumber(num1 ,num2 ,num3));
+            Console.ReadKey();
+        }
+    }
+}
