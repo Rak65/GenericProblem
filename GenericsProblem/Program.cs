e9c0baaaf373e790485dbfde9ac19bc04b825fef
Rak65 <rakeshp6505@gmail.com>
2023-05-13 19:21:07 +0530

    Given max number at Second Position return the same number

--- GenericsProblem/Program.cs
+++ GenericsProblem/Program.cs
@@ -10,8 +10,8 @@ namespace GenericsProblem
     {
         public static void Main(string[] args)
         {
-            float num1 = 80.64F;
-            float num2 = 58F;
+            float num1 = 8F;
+            float num2 = 58.43F;
             float num3 = 26F;
             Console .WriteLine ("Maximum number is "+ FindMaximum.MaxNumber(num1 ,num2 ,num3));
             Console.ReadKey();
