af3fe6965089b845fa05376db1d91a14126a5f37
Rakesh kumar <67684725+Rak65@users.noreply.github.com>
2023-05-24 19:45:40 +0530

    Update FindMaximum.cs

--- GenericsProblem/FindMaximum.cs
+++ GenericsProblem/FindMaximum.cs
@@ -10,15 +10,15 @@ namespace GenericsProblem
     {
         public static float  MaxNumber(float num1, float num2, float num3)
         {
-            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 || num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 || num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
             {
                 return num1;
             }
-            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 || num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 || num2.CompareTo(num2) > 0 && num2.CompareTo(num3) >= 0)
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
             {
                 return num2;
             }
-            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 || num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 || num3.CompareTo(num2) > 0 && num3.CompareTo(num2) >= 0)
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
             {
                 return num3;
             }
