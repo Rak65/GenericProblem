f64b6f1575394a61db805de6635ad92f490283e0
Rakesh kumar <67684725+Rak65@users.noreply.github.com>
2023-05-24 19:48:05 +0530

    Update FindMaximum.cs

--- GenericsProblem/FindMaximum.cs
+++ GenericsProblem/FindMaximum.cs
@@ -10,16 +10,15 @@ namespace GenericsProblem
     {
         public static string MaxString(string str1, string str2, string str3)
         {
-            if (str1.CompareTo(str2) > 0 && str1.CompareTo(str3) > 0 || str1.CompareTo(str2) >= 0 && str1.CompareTo(str3) > 0 || str1.CompareTo(str2) > 0 && str1.CompareTo(str3) >= 0)
+            if (str1.CompareTo(str2) > 0 && str1.CompareTo(str3) > 0)
             {
                 return str1;
             }
-            if (str2.CompareTo(str1) > 0 && str2.CompareTo(str3) > 0 || str2.CompareTo(str1) >= 0 && str2.CompareTo(str3) > 0 || str2.CompareTo(str2) > 0 && str2.CompareTo(str3) >= 0)
+            if (str2.CompareTo(str1) > 0 && str2.CompareTo(str3) > 0)
             {
                 return str2;
             }
-            if (str3.CompareTo(str1) > 0 && str3.CompareTo(str2) > 0 || str3.CompareTo(str1) >= 0 && str3.CompareTo(str2) > 0 || str3.CompareTo(str2) > 0 && str3.CompareTo(str2) >= 0)
-            {
+            if (str3.CompareTo(str1) > 0 && str3.CompareTo(str2) > 0)
                 return str3;
             }
             return str1;
